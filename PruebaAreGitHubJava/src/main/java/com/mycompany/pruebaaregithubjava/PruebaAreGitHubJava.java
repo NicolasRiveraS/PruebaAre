7e8adb42253ac340c697ec4ebafaa4118d4b6984
NicolasRiveraS <nicolasrivera0306@gmail.com>
2024-02-09 20:54:58 -0600

    Subida del primer proyecto de Are

---
+++ PruebaAreGitHubJava/src/main/java/com/mycompany/pruebaaregithubjava/PruebaAreGitHubJava.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaaregithubjava;
+
+/**
+ *
+ * @author onicg
+ */
+public class PruebaAreGitHubJava {
+
+    public static void main(String[] args) {
+        // Hola mi amor
+        //Te amo <3
+        
+        System.out.println("Hello World!");
+    }
+}
